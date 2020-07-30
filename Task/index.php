c21141d802eb2a6a037d190a25bf25eeb3b9039f
abdullarayyan <56296444+abdullarayyan@users.noreply.github.com>
2020-07-30 14:44:39 +0300

    Using abstract in process of logging Task

--- Task/index.php
+++ Task/index.php
@@ -1,46 +1,135 @@
 <?php
-session_start();
-if($_SERVER['REQUEST_METHOD']=='POST'){
-    include('sqlconn.php');
-if(empty($_POST['username'])){
-    header('location:login.php?Empty=plz fill username');
-
-}else{
+    session_start();
+    global $id,$username;
     $user= $_POST['username'];
-    $password=$_POST['password'];
-    $_SESSION["username"] = $user;
-    $_SESSION["password"] = $password;
-    //print_r($_SESSION);
-    //$_SESSION['secsess']='You Are Welcome'.$user;
-    $result = $mysqli->query("SELECT id, username, password FROM user WHERE username = '$user' AND password = '$password' LIMIT 1");
-    if ($result->num_rows != 0) {
-        $row = $result->fetch_assoc();
-        $id = $row['id'];
-        $username = $row['username'];
-        $password = $row['password'];
-        if ($id == 0) {
-       //     echo "logged in welcome admin"."<br>";
-       // echo  "<a href='controllpanle.php'>add user</a>";
-    }
-        else {
+    //$id  = $_POST['id'];    
+abstract class checkuser{
 
-            //echo "welcome user" .'<br>' .$username;
+    abstract public function check($usertype);
+}
+
+
+
+
+
+
+
+class admin extends checkuser{
+   // public $id;
+  // public $user;
+
+     public function check($usertype){
+        include('sqlconn.php');
+
+        global $id,$user;
+        $password=$_POST['password'];
+        $_SESSION["username"] = $user;
+            $this->id=$usertype;
+            $result = $mysqli->query("SELECT id FROM user WHERE username = '$user' AND password = '$password' LIMIT 1");
+            if ($result->num_rows != 0) {
+                $row = $result->fetch_assoc();
+                $id = $row['id'];
+            if ($id ==0 ) {
+                echo '<li><a href="controllpanle.php"><i class="fas fa-plus"></i>Add New User</a></li>';
+                     }
+
+}
+}}
+
+
+class user extends checkuser{
+ 
+ 
+      public function check($usertype){
+         include('sqlconn.php');
+ 
+         global $id,$user;
+         $password=$_POST['password'];
+         $_SESSION["username"] = $user;
+             $this->id=$usertype;
+             $m = $mysqli->query("SELECT id FROM user WHERE username = '$user' AND password = '$password' LIMIT 1");
+             if ($m->num_rows != 0) {
+                 $row = $m->fetch_assoc();
+                 $id = $row['id'];
+             if ($id !=0 ) {
+             echo '<li><a href="#"><i class="fas fa-home"></i>welcome  User</a></li>';
+          
+             }
+ 
+ }
+ }}
+
+
+
+
+
+
+
+
+
+
+
+ class loginuser{
+    public $user;
+
+    public function login($usertype){
+
+        global $user;
+        include('sqlconn.php');
+        $_SESSION["username"] = $user;
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+
+            if(empty($_POST['username'])){
+                header('location:login.php?Empty=plz fill username');
+            }else{
+                        $this->user=$usertype;
+                        $password=$_POST['password'];
+                        $_SESSION["username"] = $user;
+                        $_SESSION["password"] = $password;
+                        //$_SESSION["id"]       =$id;  
+                    $result = $mysqli->query("SELECT id, username, password FROM user WHERE username = '$user' AND password = '$password' LIMIT 1");
+                    if ($result->num_rows != 0) {
+                        $row = $result->fetch_assoc();
+                        $id = $row['id'];
+                        $username = $row['username'];
+                        $password = $row['password'];
         }
-    }
-    else
-    {
+         else{
         header('location:login.php');
 
         echo "<script>alert('invalid username or password, Try Again!')</script>";
 
-    }
+    
+}}}
+    else{header('location:login.php');}
+   }}
+     
 
-}}else
 
-{
+   echo "<pre>";
 
-  header('location:login.php');
-}
+
+$login=new loginuser();
+$login->login($user);
+
+
+//print_r($login);
+echo"</pre>";
+
+echo "<pre>";
+
+$admin=new admin();
+//$admin->check($id);
+//print_r($admin);
+
+echo"</pre>";
+
+echo "<pre>";
+$user1=new user();
+//$user1->check($id);
+//print_r($user1);
+echo"</pre>";
+
 
 ?>
 <!DOCTYPE html>
@@ -54,26 +143,25 @@ if(empty($_POST['username'])){
 <body>
 
 <div class="wrapper">
-    <div class="sidebar" value="<?php echo $username; ?>">
+
+    <div class="sidebar" value="<?php  echo $username; ?>">
         <h2><?php  if (isset($_SESSION['username'])) {echo"".$user;} ?></h2>
-       
+
         <ul>
+        
         <?php  if (isset($_SESSION['username'])) {
-                    if ($id == 0) {
-                       echo '<li><a href="controllpanle.php"><i class="fas fa-plus"></i>Add New User</a></li>';
-
-                    }else{
-                        echo '<li><a href="#"><i class="fas fa-home"></i>welcome  User</a></li>';
-                       }
-
-            } ?>
+                        $admin->check($id);
+                       $user1->check($id);
+           }  ?>
 
             <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i>Log Out</a></li>
-         
+
+
         </ul> 
+
     </div>
     <div class="main_content">
-        <div class="header"><?php  if (isset($_SESSION['username'])) {echo"You Are Welcome  "."<bre>".$user;} ?></div>  
+        <div class="header"><?php  if (isset($_SESSION['username'])) {echo"You Are Welcome  "."  ".$user;} ?></div>  
         <div class="info">
     
         <div>       </div>
